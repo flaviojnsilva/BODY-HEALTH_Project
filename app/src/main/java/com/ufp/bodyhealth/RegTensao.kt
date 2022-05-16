23787dfc5d40a22bbcea14fe1facac68971bc21e
anogueira <37370@ufp.edu.pt>
2022-05-16 23:27:39 +0100

    new act tensoes

---
+++ app/src/main/java/com/ufp/bodyhealth/RegTensao.kt
@@ -0,0 +1,11 @@
+package com.ufp.bodyhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegTensao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_tensao)
+    }
+}
