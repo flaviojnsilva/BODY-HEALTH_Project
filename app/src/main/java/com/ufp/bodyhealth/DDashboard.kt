d40698ef9cf36d938f3ae26aaabac5789ee8a664
anogueira <37370@ufp.edu.pt>
2022-05-18 20:58:51 +0100

    Screens de Diabetes e Tensoes inseridos

--- app/src/main/java/com/ufp/bodyhealth/DDashboard.kt
+++ app/src/main/java/com/ufp/bodyhealth/DDashboard.kt
@@ -3,9 +3,9 @@ package com.ufp.bodyhealth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegTensao : AppCompatActivity() {
+class DDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reg_tensao)
+        setContentView(R.layout.activity_ddashboard)
     }
 }
---
+++ app/src/main/java/com/ufp/bodyhealth/TDashboard.kt
@@ -0,0 +1,11 @@
+package com.ufp.bodyhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tdashboard)
+    }
+}
